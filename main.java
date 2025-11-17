62c13efbcac46e11b4015efa477fc6aa0f1d5f64
mariamatanji <38054680+mariamatanji@users.noreply.github.com>
2025-11-17 12:21:28 -0800

    Implement Pig Latin conversion in Main class

---
+++ main.java
@@ -0,0 +1,46 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+    public static String makeCluster(String s){
+      String cluster = ""; 
+      int i=0;
+      while(!isVowel(s.charAt(i))){
+        cluster = cluster + s.charAt(i);
+        i++;
+          
+      }
+        return cluster;
+        
+        
+    }
+    public static boolean isVowel(char c){
+        return (c == 'a'|| c == 'e' ||c == 'o' ||c == 'u'|| c == 'i');
+    }
+    public static String pigLatin(String s){
+        String answer;
+        String cluster = makeCluster(s);
+        if(isVowel(s.charAt(0))){
+        answer = s + "-way";
+        }
+        else
+            answer = s.substring(cluster.length())+ "-" + cluster +"ay";
+            return answer;
+        
+        
+    }
+    
+	public static void main(String[] args) {
+	    String input = "Speak";
+	    //String answer = makeCluster(input);
+	    String answer = pigLatin(input);
+	    System.out.println(answer);
+		
+	}
+}
